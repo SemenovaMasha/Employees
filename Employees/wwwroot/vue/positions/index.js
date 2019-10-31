ef8a0a440310c1a53493ee58b836a3308993c522
Mariya Semenova <mariya.semenova@simbirsoft.com>
2019-10-31 18:31:03 +0400

    b-table

--- Employees/wwwroot/vue/positions/index.js
+++ Employees/wwwroot/vue/positions/index.js
@@ -6,4 +6,26 @@
     
 //     `
 //   })
-new Vue({ el: '#Positions' })
+new Vue({ el: '#Positions' ,
+data: {
+  filters: {
+    id: '',
+    issuedBy: '',
+    issuedTo: ''
+  },
+  items: [{id:1234,issuedBy:'Operator',issuedTo:'abcd-efgh'},{id:5678,issuedBy:'User',issuedTo:'ijkl-mnop'}]
+},
+computed: {
+  filtered () {
+    const filtered = this.items.filter(item => {
+      return Object.keys(this.filters).every(key =>
+          String(item[key]).includes(this.filters[key]))
+    })
+    return filtered.length > 0 ? filtered : [{
+      id: '',
+      issuedBy: '',
+      issuedTo: ''
+    }]
+  }
+}
+})
--- Employees/wwwroot/vue/positions/list.js
+++ Employees/wwwroot/vue/positions/list.js
@@ -1,6 +1,12 @@
 ﻿export default Vue.component('list', {
+  props: ["filtered"],
     template: `
+    <div>
     <div>test</div>
-    
+    <b-table striped show-empty :items="filtered">
+ 
+</b-table>
+
+</div>
     `
   })
