3d9d771743a4b29823c4382f3ae1d455cff36377
Mariya Semenova <mariya.semenova@simbirsoft.com>
2019-11-06 18:16:43 +0400

    GetWorkingDays

--- Employees/Services/ReportsService.cs
+++ Employees/Services/ReportsService.cs
@@ -154,6 +154,15 @@ where timeSum.sumEstimated < timeSum.sumElapsed
 order by fio
 ";
         }
+
+        public int GetWorkingDays(DateTime from, DateTime to)
+        {
+            var dayDifference = (int)to.Subtract(from).TotalDays;
+            return Enumerable
+                .Range(1, dayDifference)
+                .Select(x => from.AddDays(x))
+                .Count(x => x.DayOfWeek != DayOfWeek.Saturday && x.DayOfWeek != DayOfWeek.Sunday);
+        }
     }
 
 }
